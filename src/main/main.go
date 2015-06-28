0b11b5e4daba777b228d5f1383ad359dba59a54d
Gavin <gavin@apple.local>
2015-06-28 13:02:09 +0800

    support the static files

--- src/main/main.go
+++ src/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"io/ioutil"
+	"strings"
 	"log"
 )
 
@@ -10,11 +11,37 @@ type Myhandler struct  {
 }
 
 func (this *Myhandler) ServeHTTP(w http.ResponseWriter,r *http.Request) {
-	paths := r.URL.Path[0:]
+	paths := r.URL.Path[1:]
 	log.Println(paths)
 
-	data,err:=ioutil.ReadFile("template"+paths)
+	var filePath string
+	if strings.HasPrefix(paths,"public"){
+		filePath = paths
+	}else{
+		filePath = "template/"+paths
+	}
+	log.Println(filePath)
+	data,err:=ioutil.ReadFile(filePath)
+
 	if err == nil {
+		var contentType string
+		if strings.HasSuffix(paths,".css"){
+			contentType = "text/css"
+		}else if strings.HasSuffix(paths,".js"){
+			contentType = "application/javascript"
+		}else if strings.HasSuffix(paths,".png"){
+			contentType = "image/png"
+		}else if strings.HasSuffix(paths,".html"){
+			contentType = "text/html"
+		/*}else if strings.HasSuffix(paths,""){
+			contentType = "text/css"
+		}else if strings.HasSuffix(paths,".css"){*/
+
+		}else{
+			contentType = "text/plain"
+		}
+
+		w.Header().Add("content type",contentType)
 		w.Write(data)
 	} else {
 		w.WriteHeader(404)
